5f45cdf276c7a66fcafaf9f787161f4f94b71316
Halil Bilgil <hbilgil@gmail.com>
2022-06-20 17:34:18 +0300

    Edit script.js to declare library class

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@ class Book { //class constructor for books
     this.status = status
   }
 }
+
+class Library { //class constructor for library
+  constructor() {
+    this.books = [] //empty array of books
+  }
+
+  getBook(title) { //is used to revoke functions outside the class to find the exact book
+    return this.books.find((book) => book.title === title)
+  }
+
+  addBook(newBook) { //is used to revoke functions outside the class in order to add new books to the library
+    if (!this.searchInLibrary(newBook)) {
+      this.books.push(newBook)
+    }
+  }
+
+  removeBook(title) { // is used to revoke functions outside the class in order to remove books from the library
+    this.books = this.books.filter((book) => book.title !== title)
+  }
+
+  searchInLibrary(newBook) { // is used to revoke functions outside the class in order to make comparisons
+    return this.books.some((book) => book.title === newBook.title)
+  }
+
+  deleteBooksInLibrary() { // is used to revoke functions outside the class in order to COMPLETELY remove ALL books from the library
+    this.books = [];
+  }
+}
